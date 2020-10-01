5b28149a8d55421ba71b0265505a48b801c294d8
Chahat <44916363+colourful-breeze19@users.noreply.github.com>
2020-10-01 22:41:29 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,17 @@
+//string
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{ char a[20];
+int length;
+cout<<"enter your name \n";
+cin>>a;
+length=strlen(a);
+cout<<"length of string "<< length ;
+cout<<"\n hello!"<< a;
+
+
+
+    return 0;
+}
